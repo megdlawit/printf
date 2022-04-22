04625cbe4b36db271a2ac1e02cd1f938cc53a857
megdlawit anbessie <megdalenea@gmail.com>
2022-04-22 09:13:48 +0300

    mmmm

---
+++ get_sign.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * get_sign - turns on flags if _printf finds
+ * a flag modifier in the format string
+ * @s: character that holds the flag specifier
+ * @f: pointer to the struct flags in which we turn the flags on
+ *
+ * Return: 1 if a flag has been turned on, 0
+ */
+
+
+int get_sign(char s, sign_t *f)
+{
+	int i = 0;
+
+	switch (s)
+	{
+	case '+':
+		f->plus = 1;
+		i = 1;
+		break;
+	case ' ':
+		f->space = 1;
+		i = 1;
+		break;
+	case '#':
+		f->hash = 1;
+		i = 1;
+		break;
+	}
+	return (i);
+
+}
